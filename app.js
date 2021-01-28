d7de424d3138a30b909aec1daab75c9b1f1e9ed8
Johan Gavelan <johangavelan@gmail.com>
2021-01-27 23:52:57 -0500

    get movie by genres done

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const categoriesEl = document.querySelector('.categories');
 const API_KEY = '659755775c442c8c41820f55809c3842';
 const MOVIE_ENDPOINT = 'https://api.themoviedb.org'
 //query
-let searchQuery = 'batman';
+let searchQuery = 'into';
 
 //helper functions
 const getYear = (fullDate) => new Date(fullDate).getFullYear();
@@ -60,13 +60,35 @@ const getGenres = async () => {
 
     data.genres.forEach(genre => {
         markup += `
-            <li data-id="${genre.id}"><a href="#">${genre.name}</a></li>
+            <li>
+                <i class="fas fa-cubes"></i>
+                <a class="category" data-id="${genre.id}" href="#">${genre.name}</a>
+            </li>
         `
     })
     categoriesEl.innerHTML = markup;
 }
 
-getGenres()
+getGenres();
+
+const getMoviesByGenre = async (ID) => {
+    const GENRES_URL = `${MOVIE_ENDPOINT}/3/discover/movie?api_key=${API_KEY}&with_genres=${ID}`;
+    const response = await fetch(GENRES_URL);
+    const data = await response.json();
+    const movies = [];
+
+    data.results.forEach(movie => {
+        const imagePath = `https://image.tmdb.org/t/p/w400/${movie.poster_path}`;
+        const obj = {
+            id: movie.id,
+            title: movie.title,
+            image: imagePath,
+        }
+        movies.push(obj);
+    });
+
+    render(movies);
+}
 
 const render = (movies) => {
     let markup = '';
@@ -94,6 +116,13 @@ const openModal = (movieObj) => {
     setValue(review, movieObj.review);
     setValue(release, movieObj.release)
 }
+
+categoriesEl.addEventListener('click', function(event) {
+    if(event.target.matches('.category')) {
+        const genre_id = event.target.dataset.id;
+        getMoviesByGenre(genre_id);
+    }
+})
 
 searchEl.addEventListener('keypress', event => {
     if(event.keyCode === 13) {
